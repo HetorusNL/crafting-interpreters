bb714923b266190938a33c2647c6a2ffa53464ba
HetorusNL <tim@hetorus.nl>
2024-05-09 13:21:46 +0200

    removed boilerplate / hello-world stuff

--- src/java/nl/hetorus/craftinginterpreters/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package nl.hetorus.craftinginterpreters;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        // woo we have a hello world!
-        System.out.println("Hello world!");
-    }
-}
